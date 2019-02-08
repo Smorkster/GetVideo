2f902ea06ad5528cc5a988b8c98110cd30ebf043
Smorkster <smorkster@gmail.com>
2019-02-08 10:28:21 +0100

    Version 5
    Corrected how title is renamed in videoinfo object
    Corrected fething of YouTube-uploader
    Changed color for the name when starting download
    Now rounds the freespace information to two decimals

--- GetVideo synchronously.ps1
+++ GetVideo synchronously.ps1
@@ -1,13 +1,13 @@
 <#
 .SYNOPSIS
-	Reads a file of YouTube-links and downloads the videos
+Reads a file of YouTube-links and downloads the videos
 .DESCRIPTION
-	Version 4
+	Version 3
 	This script reads a file containing a list of addresses with videos that wants to be downloaded. It is mainly created for YouTube, but the application YouTube-dl can also handle other sites.
 	Before downloading, the script checks of there are (persumed) enough free space on the disc. It then continues with fetching information about the video then starts downloading.
 	If the video is located on YouTube the video will be marked as watched if the user have provided login credentials.
 	The videos will be named as such:
-		YouTube: <date of publication> <YouTube-username> <videoname>
+		YouTube: <YouTube-username> <date of publication> <videoname>
 		Other sites: <Domain-name> <videoname>
 #>
 
@@ -20,7 +20,7 @@ function GetVideoInfo($url){
 	$request = (Invoke-WebRequest -Uri $url)
 	if($videolink -match "www.youtube.com")
 	{
-		$user = ($request.Links | ? {$_.href -like "*/channel/*"}).innerText[0]
+		$user = (($request.Links | ? {$_.href -like "*/channel/*"} | ? {$_.innertext -eq $_.innerhtml})[0]).innerhtml
 		$title = ($request.ParsedHtml.title).TrimEnd(" - YouTube")
 		$published = (($request.ToString() -split '\n' | Select-String 'Published') -split "content=""")[1].Substring(0,10)
 	} else {
@@ -28,15 +28,15 @@ function GetVideoInfo($url){
 		$user = ([System.Uri] $url).Host -replace '^www\.'
 		$published = ""
 	}
-	$title = $title -replace "<" , ""
-    $title = $title -replace ">" , ""
-    $title = $title -replace ":" , " -"
-    $title = $title -replace [char]0x22 , ""
-    $title = $title -replace "/" , "_"
-    $title = $title -replace "\\" , ""
-    $title = $title -replace "\|" , "_"
-    $title = $title -replace "\?" , ""
-    $title = $title -replace "\*" , ""
+	$title = $title -replace "<" , ""			# Char <
+    $title = $title -replace ">" , ""			# Char >
+    $title = $title -replace ":" , " -"			# Char :
+    $title = $title -replace [char]0x22 , "'"	# Char "
+    $title = $title -replace "/" , "_"			# Char /
+    $title = $title -replace "\\" , ""			# Char \
+    $title = $title -replace "\|" , "_"			# Char |
+    $title = $title -replace "\?" , ""			# Char ?
+    $title = $title -replace "\*" , "_"			# Char *
 
 	Add-Member -InputObject $video -MemberType NoteProperty -Name User -Value $user
 	Add-Member -InputObject $video -MemberType NoteProperty -Name Published -Value $published
@@ -68,12 +68,12 @@ $youtubeUser = Get-Credential
 $ErrorActionPreference = "Continue"
 $filename = Read-Host "Filename"
 $links = Get-Content $filename
-$lowerRemaindingDiscLimit = 1.5
+$lowerRemaindingDiscLimit = 2
 $ticker = 1
 
 if($links -eq $null)
 {
-	Write-Host "File with links empty. Quiting" -ForegroundColor Cyan
+	Write-Host "File with links is empty. Quiting" -ForegroundColor Cyan
 	return
 }
 
@@ -96,15 +96,16 @@ for(; $start -lt $links.Count; $start++)
 		
 			if($youtubeUser -eq $null)
 			{
-				Start-Job -ScriptBlock {param($videolink,$videoname,$videotitle) c:\users\6g1w\appdata\w\youtube-dl.exe $videolink -o $videoname --no-playlist -q; if((Get-ChildItem c:\users\6g1w\appdata\w -filter *$videoTitle*).count -eq 0) { throw $videoTitle +"`n`t"+ $videolink +"`n`t"+ [int[]][char[]]$videoTitle } } -ArgumentList $videolink,$videoname,$videoinfo.Title > $null
+				Start-Job -ScriptBlock {param($videolink,$videoname,$videotitle) c:\users\6g1w\appdata\w\youtube-dl.exe $videolink -o $videoname --no-playlist -q; if((Get-ChildItem c:\users\6g1w\appdata\w -filter *$videoTitle*).count -eq 0) { throw $videoTitle +"`n`t"+ $videolink } } -ArgumentList $videolink,$videoname,$videoinfo.Title > $null
 			} else {
-				Start-Job -ScriptBlock {param($videolink,$videoname,$youtubeUserName,$youtubeUserPassword,$videoTitle) c:\users\6g1w\appdata\w\youtube-dl.exe $videolink -o $videoname -u $youtubeUserName -p $youtubeUserPassword --mark-watched --no-playlist -q; if((Get-ChildItem c:\users\6g1w\appdata\w -filter *$videoTitle*).count -eq 0) { throw $videoTitle +"`n`t"+ $videolinkv } } -ArgumentList $videolink, $videoname,$youtubeUser.UserName,$youtubeUser.GetNetworkCredential().Password,$videoinfo.Title > $null
+				Start-Job -ScriptBlock {param($videolink,$videoname,$youtubeUserName,$youtubeUserPassword,$videoTitle) c:\users\6g1w\appdata\w\youtube-dl.exe $videolink -o $videoname -u $youtubeUserName -p $youtubeUserPassword --mark-watched --no-playlist -q; if((Get-ChildItem c:\users\6g1w\appdata\w -filter *$videoTitle*).count -eq 0) { throw $videoTitle +"`n`t"+ $videolink } } -ArgumentList $videolink, $videoname,$youtubeUser.UserName,$youtubeUser.GetNetworkCredential().Password,$videoinfo.Title > $null
 			}
 		} else {
-			$videoname = "c:\users\6g1w\appdata\w\"+$videoinfo.user+ " %(title)s.%(ext)s"
-			Start-Job -ScriptBlock {param($videolink,$videoname,$videoTitle) c:\users\6g1w\appdata\w\youtube-dl.exe $videolink -o $videoname -q;if((Get-ChildItem c:\users\6g1w\appdata\w -filter *$videoTitle*).count -eq 0) { throw $videoTitle +"`n`t"+ $videolink +"`n`t"+ [int[]][char[]]$videoTitle } } -ArgumentList $videolink,$videoname,$videoinfo.Title > $null
+			$videoname = "c:\users\6g1w\appdata\w\"+$videoinfo.user+" "+ $videoinfo.Title+".%(ext)s"
+			Start-Job -ScriptBlock {param($videolink,$videoname,$videoTitle) c:\users\6g1w\appdata\w\youtube-dl.exe $videolink -o $videoname -q;if((Get-ChildItem c:\users\6g1w\appdata\w -filter *$videoTitle*).count -eq 0) { throw $videoTitle +"`n`t"+ $videolink +"`n`t" + $videoTitle } } -ArgumentList $videolink,$videoname,$videoinfo.Title > $null
 		}
-		Write-Host "Downloading" $videoinfo.Title
+		Write-Host "Downloading" $ticker "of" $links.Count -NoNewline
+		Write-Host $videoinfo.Title -ForegroundColor Cyan
 		$videoinfo = $null
 	} else {
 		Write-Host "To little free disc space."
@@ -115,22 +116,22 @@ for(; $start -lt $links.Count; $start++)
 }
 Write-Host "Waiting for downloads `n"
 
-$fails = Get-Job | Wait-Job
+Get-Job | Wait-Job > $null
 
-Write-Host "Done downloading" $links.Count
-Write-Host "Free space" ($comp.FreeSpace / 1GB)
+Write-Host "Done downloading" $links.Count "videos"
+Write-Host "Free space:" ([math]::Round($comp.FreeSpace / 1GB,2)) "GB"
 $null > $filename
-
 $fails = Get-Job | ? {$_.State -eq 'Failed'}
-$addresses = New-Object -ComObject System.Collections.ArrayList
-if($fails.count -gt 0)
+
+if($fails.Count -gt 0)
 {
+	$addresses = New-Object -ComObject System.Collections.ArrayList
 	Write-Host "These failed to download"
-	foreach($j in $fails)
+	foreach($failed in $fails)
 	{
-		Write-Host $j.ChildJobs[0].JobStateInfo.Reason.Message -ForegroundColor Red
-		$addresses.Add($j.ChildJobs[0].JobStateInfo.Reason.Message.Split("`t")[1]) > $null
+		Write-Host $failed.ChildJobs[0].JobStateInfo.Reason.Message -ForegroundColor Red
+		$addresses.Add($failed.ChildJobs[0].JobStateInfo.Reason.Message.Split("`t")[1]) > $null
 	}
+	$addresses > $filename
 }
-$addresses > $filename
 Get-Job | Remove-Job
